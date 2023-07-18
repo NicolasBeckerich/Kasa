2d0e38a30fe20e2ff3faffe4469367c57df4a8df
NicolasBeckerich <beckerichnicolas.com>
2023-07-18 10:20:40 +0100

    Supression fichier et modif

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./sass/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <main>
+        <AppRouter />
+      </main>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
+        <>
             <h1>Page d'accueil</h1>
             <p>Bienvenue sur la page d'accueil !</p>
-        </div>
+        </>
     );
 }
 
