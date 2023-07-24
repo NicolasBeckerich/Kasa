5fc412edba833fa02781bf4b294e27d9f5685842
NicolasBeckerich <beckerichnicolas.com>
2023-07-24 09:51:26 +0100

    Modif text about.jsx collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,7 +18,7 @@ function About() {
         />
         <Collapse
           title="Service"
-          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de note plateforme."
         />
         <Collapse
           title="Sécurité"
