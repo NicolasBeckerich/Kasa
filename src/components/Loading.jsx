5893c2ba632a25653e606d6f841ebdb514e96850
NicolasBeckerich <beckerichnicolas.com>
2023-09-30 14:52:01 +0100

    Updates

--- src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-// J'importe React
-import React from "react";
-
-// Je défini le composant loading qui permet de faire un flèche qui tourne de chargement qui tourne
-
-const Loading = () => <div className="loader"></div>;
-
-//J'exporte Loading pour qu'il puisse être utilisé dans d'autres parties de l'application.
-export default Loading;
