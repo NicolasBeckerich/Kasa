4ebfd1cc0c96e7e48b44268de89fd7f135da9573
NicolasBeckerich <beckerichnicolas.com>
2023-09-30 15:07:23 +0100

    Dernière modif projets

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 //j'importe les routes de react-router-dom et les composants de chaque page
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Logement from "../pages/Logement";
@@ -8,7 +8,6 @@ import PageNotFound from "../pages/PageNotFound";
 // Mon AppRouter va définir les routes de l'application
 function AppRouter() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       {/* pour / j'arrive sur la page Home */}
       <Route path="/" element={<Home />} exact />
@@ -22,7 +21,6 @@ function AppRouter() {
       {/* pour toutes les autres route j'arrive sur PageNotFound */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>
-    </Router>
   );
 }
 
